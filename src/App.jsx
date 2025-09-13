9b09eda34ad56ac2e399507cb1d075af56def4d5
samkc-0 <sam.k.c.0@icloud.com>
2025-09-13 12:39:45 +0100

    add game code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import styled from "styled-components";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Colors = {
+  INACTIVE: "red",
+  ACTIVE: "lime",
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const Block = styled.div`
+  background-color: ${(props) => props.color};
+  width: 100px;
+  height: 100px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  border-radius: 5px;
+  padding: 1rem;
+`;
+
+function randomDurationInSeconds(lower, upper) {
+  return lower + Math.random() * (upper - lower);
 }
 
-export default App
+function App() {
+  const [gameOver, setGameOver] = useState(false);
+  const [color, setColor] = useState(Colors.INACTIVE);
+  const [timerStart, setTimerStart] = useState(null);
+  const [message, setMessage] = useState("");
+
+  const handleClick = () => {
+    if (color === Colors.INACTIVE) {
+      setMessage("You clicked too early!");
+    } else if (color === Colors.ACTIVE) {
+      const now = new Date();
+      const dt = now - timerStart;
+      setMessage(`You took ${dt}ms seconds.`);
+    }
+    setGameOver(true);
+  };
+
+  const changeColor = () => {
+    setColor(Colors.ACTIVE);
+    setTimerStart(new Date());
+  };
+
+  const reset = () => {
+    setGameOver(false);
+    setColor(Colors.INACTIVE);
+  };
+
+  useEffect(() => {
+    if (gameOver) return;
+    const timeout = setTimeout(
+      changeColor,
+      randomDurationInSeconds(1, 4) * 1000,
+    );
+    return () => clearTimeout(timeout);
+  }, [gameOver]);
+
+  return (
+    <Flex>
+      {gameOver ? (
+        <>
+          <Button onClick={reset}>Start Game</Button>
+          <h2>{message}</h2>
+        </>
+      ) : (
+        <Block color={color} onClick={handleClick} />
+      )}
+    </Flex>
+  );
+}
+
+export default App;
